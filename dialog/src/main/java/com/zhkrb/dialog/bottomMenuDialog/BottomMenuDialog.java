6a7619c341be25f8bd623f3a7683f55721d57684
zhkrb <fiybad1@gmail.com>
2021-02-19 18:12:46 +0800

    2021.02.19 temp update

--- dialog/src/main/java/com/zhkrb/dialog/bottomMenuDialog/BottomMenuDialog.java
+++ dialog/src/main/java/com/zhkrb/dialog/bottomMenuDialog/BottomMenuDialog.java
@@ -1,10 +1,14 @@
 package com.zhkrb.dialog.bottomMenuDialog;
 
 import android.app.Dialog;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
 
 import com.zhkrb.dialog.R;
 import com.zhkrb.dialog.base.AbsDialog;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * @description：
@@ -12,6 +16,9 @@ import com.zhkrb.dialog.base.AbsDialog;
  * @DATE： 2021/2/13 23:37
  */
 public class BottomMenuDialog extends AbsDialog {
+
+    private RecyclerView mRecyclerView;
+    private TextView mTextTitle;
 
 
     @Override
@@ -34,11 +41,22 @@ public class BottomMenuDialog extends AbsDialog {
 
     @Override
     protected void bindView(View view) {
+        mRecyclerView = view.findViewById(R.id.recycler_view);
+        mTextTitle = view.findViewById(R.id.text_title);
 
     }
 
     @Override
     protected void main() {
+        if (!TextUtils.isEmpty(mController.getTitle())){
+            mTextTitle.setVisibility(View.VISIBLE);
+            mTextTitle.setText(mController.getTitle());
+        }else {
+            mTextTitle.setVisibility(View.GONE);
+        }
+
+
+
 
     }
 
